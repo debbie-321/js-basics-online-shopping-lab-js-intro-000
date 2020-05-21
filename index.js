6b6349f9f5d35915f884c8399929da9a0fdfc307
Deborah Nakakande <d.nakakande@hotmail.com>
2020-05-21 16:04:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function setCart(c) {
 function addToCart(item) {
  // write your code here
   var price = Math.floor(Math.random() * 101)
+
   var itemObj = {
     "itemName": item,
     "itemPrice": price
@@ -20,11 +21,20 @@ function addToCart(item) {
   cart.push(itemObj)
   return `${item} has been added to your cart.`
   return cart
-
 }
 
 function viewCart() {
   // write your code here
+  for (i = 0; i < cart.length; i++){
+    if (cart.length === 0){
+      return `Your shopping cart is empty.`
+    } else if (cart.length === 1){
+      return `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}.`
+    }else if (cart.length === 2){
+      return `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}, ${cart[1].itemName} at ${cart[1].itemName}`
+    }
+    }
+  }
 }
 
 function total() {
